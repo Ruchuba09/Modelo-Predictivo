4527e376ef88e3177a796088ed4f5549aebfb3a5
Aesedefe1 <67512673+Aesedefe1@users.noreply.github.com>
2026-09-04 09:18:16 -0600

    asd prueba
    asd.tsx en js/pages como prueba update al docker

---
+++ resources/js/pages/asd.tsx
@@ -0,0 +1,10 @@
+// resources/js/Pages/Usuarios.tsx
+
+export default function asd() {
+    return (
+        <div>
+            <h1>Usuarios</h1>
+            <p>Esta es la vista de usuarios.</p>
+        </div>
+    );
+}
