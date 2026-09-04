517376872269c4f34c274cfde5c0b5986a329136
Aesedefe1 <67512673+Aesedefe1@users.noreply.github.com>
2026-09-03 21:43:03 -0600

    Primera vista
    Integracion la db en postgresql y una vista de prueba

---
+++ resources/js/pages/Usuarios.jsx
@@ -0,0 +1,10 @@
+// resources/js/Pages/Usuarios.tsx
+
+export default function Usuarios() {
+    return (
+        <div>
+            <h1>Usuarios</h1>
+            <p>Esta es la vista de usuarios.</p>
+        </div>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/usuarios', function () {
+    return Inertia::render('Usuarios');
+});
+
 Route::middleware(['auth'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
