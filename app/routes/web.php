208f7be0eadde8ff4f7d1dbf48e6b93b2699b281
Nicolas Urrutia <currutiad@ing.ucsc.cl>
2026-09-11 17:42:35 -0300

    Se agrega rutas para entrar sin cuenta a las páginas creadas

--- app/routes/web.php
+++ app/routes/web.php
@@ -15,11 +15,22 @@ Route::get('/login2', function () {
 Route::resource('trabajadores', TrabajadorController::class)
     ->parameters(['trabajadores' => 'trabajador']);
     
-Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    Route::get('/portal', function () {
+        return Inertia::render('portal');
+    });
+    
+Route::get('dashboard', function () {
+    return Inertia::render('dashboard');
+})->name('dashboard');
+
+Route::get('/usuarios', function () {
+    return Inertia::render('usuarios');
+})->name('usuarios');
+
+Route::get('/reportes', function () {
+    return Inertia::render('reportes');
 });
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
