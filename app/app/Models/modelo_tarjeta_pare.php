80b4ba8ec30730d4534153bafba467bf982b3a61
Aesedefe1 <67512673+Aesedefe1@users.noreply.github.com>
2026-09-12 21:29:58 -0600

    sab4

--- app/app/Models/modelo_tarjeta_pare.php
+++ app/app/Models/modelo_tarjeta_pare.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class modelo_tarjeta_pare extends Model
 {
-    protected $connection = "public";
+    protected $connection = "usuarios";
     protected $table = "modelo_tarjeta_pare";
     protected $fillable = ["nombre", "descripcion"];
     public $timestamps = false;
